a7d452f0cf62b21659a4f9d2d2dfb14a52877343
JustineLopez <jus.paololopez@gmail.com>
2024-04-29 13:09:08 +0800

    update ALLOWED_HOSTS

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$)rme3*9yd(&=k!sykh9*@-uu7rz8^ucu8%ggc*a^#$cx*_&r@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mydjangoapp.pythonanywhere.com']
 
 
 # Application definition
